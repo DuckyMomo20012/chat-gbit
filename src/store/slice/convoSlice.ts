4a95e63d267585b82bac0f17a27b3f92528710bc
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-03-06 20:05:09 +0700

    chore: remove incorrect convo initialState

--- src/store/slice/convoSlice.ts
+++ src/store/slice/convoSlice.ts
@@ -2,51 +2,7 @@ import { createSlice, nanoid } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 import type { TChat } from '@/components/elements/Message';
 
-const initialState: Array<TChat> = [
-  {
-    id: '1',
-    type: 'prompt',
-    text: 'Hello',
-    isTyping: false,
-  },
-  {
-    id: '2',
-    type: 'completion',
-    text: 'Yes, using the "clean all" command in DiskPart is a way to perform a hard format or low-level format of an SSD. This command will overwrite the entire SSD with zeroes, effectively erasing all data and restoring the drive to its original state. It\'s important to note that the "clean all" command is a powerful tool that should be used with caution. Once the command is executed, all data on the drive will be irretrievably lost. Additionally, the process can be time-consuming and may take several hours to complete depending on the size of the SSD ',
-    isTyping: false,
-  },
-
-  // {
-  //   id: '3',
-  //   type: 'prompt',
-  //   text: 'Hello',
-  // },
-  // {
-  //   id: '4',
-  //   type: 'completion',
-  //   text: 'Yes, using the "clean all" command in DiskPart is a way to perform a hard format or low-level format of an SSD. This command will overwrite the entire SSD with zeroes, effectively erasing all data and restoring the drive to its original state. It\'s important to note that the "clean all" command is a powerful tool that should be used with caution. Once the command is executed, all data on the drive will be irretrievably lost. Additionally, the process can be time-consuming and may take several hours to complete depending on the size of the SSD ',
-  // },
-  // {
-  //   id: '5',
-  //   type: 'prompt',
-  //   text: 'Hello',
-  // },
-  // {
-  //   id: '6',
-  //   type: 'completion',
-  //   text: 'Yes, using the "clean all" command in DiskPart is a way to perform a hard format or low-level format of an SSD. This command will overwrite the entire SSD with zeroes, effectively erasing all data and restoring the drive to its original state. It\'s important to note that the "clean all" command is a powerful tool that should be used with caution. Once the command is executed, all data on the drive will be irretrievably lost. Additionally, the process can be time-consuming and may take several hours to complete depending on the size of the SSD ',
-  // },
-  // {
-  //   id: '7',
-  //   type: 'prompt',
-  //   text: 'Hello',
-  // },
-  // {
-  //   id: '8',
-  //   type: 'completion',
-  //   text: 'Yes, using the "clean all" command in DiskPart is a way to perform a hard format or low-level format of an SSD. This command will overwrite the entire SSD with zeroes, effectively erasing all data and restoring the drive to its original state. It\'s important to note that the "clean all" command is a powerful tool that should be used with caution. Once the command is executed, all data on the drive will be irretrievably lost. Additionally, the process can be time-consuming and may take several hours to complete depending on the size of the SSD Yes, using the "clean all" command in DiskPart is a way to perform a hard format or low-level format of an SSD. This command will overwrite the entire SSD with zeroes, effectively erasing all data and restoring the drive to its original state. It\'s important to note that the "clean all" command is a powerful tool that should be used with caution. Once the command is executed, all data on the drive will be irretrievably lost. Additionally, the process can be time-consuming and may take several hours to complete depending on the size of the SSD Yes, using the "clean all" command in DiskPart is a way to perform a hard format or low-level format of an SSD. This command will overwrite the entire SSD with zeroes, effectively erasing all data and restoring the drive to its original state. It\'s important to note that the "clean all" command is a powerful tool that should be used with caution. Once the command is executed, all data on the drive will be irretrievably lost. Additionally, the process can be time-consuming and may take several hours to complete depending on the size of the SSD ',
-  // },
-];
+const initialState: Array<TChat> = [];
 
 const convoSlice = createSlice({
   name: 'convo',
