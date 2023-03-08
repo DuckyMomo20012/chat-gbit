4b28204116486d7e5b6b427e16c57ed11b7db09d
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-03-08 11:14:29 +0700

    feat: monitor all-time token usage

---
+++ src/store/slice/tokenSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import type { CreateCompletionResponseUsage } from 'openai';
+import { PURGE } from 'redux-persist';
+
+const initialState: CreateCompletionResponseUsage = {
+  prompt_tokens: 0,
+  completion_tokens: 0,
+  total_tokens: 0,
+};
+
+const tokenSlice = createSlice({
+  name: 'token',
+  initialState,
+  reducers: {
+    addTokens: (
+      state,
+      action: PayloadAction<CreateCompletionResponseUsage>,
+    ) => {
+      const { payload } = action;
+
+      const {
+        prompt_tokens: promptTokens,
+        completion_tokens: completionTokens,
+        total_tokens: totalTokens,
+      } = payload;
+
+      return {
+        prompt_tokens: state.prompt_tokens + promptTokens,
+        completion_tokens: state.completion_tokens + completionTokens,
+        total_tokens: state.total_tokens + totalTokens,
+      };
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(PURGE, () => {
+      return initialState;
+    });
+  },
+});
+
+export const { addTokens } = tokenSlice.actions;
+
+export default tokenSlice.reducer;
