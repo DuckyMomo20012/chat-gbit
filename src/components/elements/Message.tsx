d90331489e418a24c7326b3a1b05d999e1ef2dbc
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-03-09 00:05:10 +0700

    feat: allow to set system message & update docs (#10)
    * feat: allow to set system message (#9)
    
    * feat: allow to set system instruction
    
    * feat: update avatar for system message & add tooltip
    
    * chore: update UI
    
    * docs: update README.md

--- src/components/elements/Message.tsx
+++ src/components/elements/Message.tsx
@@ -1,3 +1,4 @@
+import { Icon } from '@iconify/react';
 import {
   Box,
   Center,
@@ -6,6 +7,7 @@ import {
   Loader,
   Avatar as MantineAvatar,
   Text,
+  Tooltip,
 } from '@mantine/core';
 import Avatar from 'boring-avatars';
 import clsx from 'clsx';
@@ -25,37 +27,48 @@ const Message = forwardRef(function Message(
     <Center
       className={clsx('w-full p-4', {
         'bg-gray-100 dark:bg-gray-700': role === 'assistant',
+        'bg-pink-100 dark:bg-pink-700': role === 'system',
       })}
     >
       <Group className="w-full md:max-w-2xl lg:max-w-2xl xl:max-w-3xl" noWrap>
-        <MantineAvatar
-          className="self-start"
-          color="indigo"
-          radius="sm"
-          size="md"
-          variant="filled"
-        >
-          {role === 'assistant' && (
-            <Image
-              alt="chatgpt-avatar"
-              height={28}
-              src="/img/chatgpt.svg"
-              width={28}
-            />
-          )}
-
-          {role === 'user' &&
-            (userName ? (
-              <Avatar
-                colors={colors}
-                name={userName}
-                size={28}
-                variant="beam"
+        <Tooltip label={role.toUpperCase()} position="left">
+          <MantineAvatar
+            className="self-start"
+            color="indigo"
+            radius="sm"
+            size="md"
+            variant="filled"
+          >
+            {role === 'system' && (
+              <Icon
+                height={28}
+                icon="material-symbols:robot-outline"
+                width={28}
               />
-            ) : (
-              <Loader size="sm" />
-            ))}
-        </MantineAvatar>
+            )}
+
+            {role === 'assistant' && (
+              <Image
+                alt="chatgpt-avatar"
+                height={28}
+                src="/img/chatgpt.svg"
+                width={28}
+              />
+            )}
+
+            {role === 'user' &&
+              (userName ? (
+                <Avatar
+                  colors={colors}
+                  name={userName}
+                  size={28}
+                  variant="beam"
+                />
+              ) : (
+                <Loader size="sm" />
+              ))}
+          </MantineAvatar>
+        </Tooltip>
         {!isTyping && <Text>{content}</Text>}
 
         {isTyping && (
