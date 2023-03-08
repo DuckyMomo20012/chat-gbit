b7dafeca0a87ac837db2c132cabbe07a3f813ccf
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-03-08 17:06:54 +0700

    feat: allow to set system instruction

--- src/components/modules/PromptForm.tsx
+++ src/components/modules/PromptForm.tsx
@@ -1,18 +1,21 @@
 import { Icon } from '@iconify/react';
-import { ActionIcon, TextInput } from '@mantine/core';
+import { ActionIcon, Checkbox, Stack, TextInput } from '@mantine/core';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 
 export type TFormData = {
   prompt: string;
+  asSystemMessage: boolean;
 };
 
 const PromptForm = ({
   isBusy,
   onSubmit,
+  allowSystemMessage = false,
 }: {
   isBusy: boolean;
   onSubmit: (data: TFormData) => unknown;
+  allowSystemMessage?: boolean;
 }) => {
   const { register, reset, handleSubmit, setFocus, formState } =
     useForm<TFormData>();
@@ -25,32 +28,41 @@ const PromptForm = ({
     if (formState.isSubmitSuccessful) {
       reset({
         prompt: '',
+        asSystemMessage: false,
       });
     }
   }, [formState.isSubmitSuccessful, reset]);
 
   return (
     <form className="w-full md:w-1/2" onSubmit={handleSubmit(onSubmit)}>
-      <TextInput
-        rightSection={
-          <ActionIcon
-            color="blue"
-            loading={isBusy}
-            size="md"
-            type="submit"
-            variant="light"
-          >
-            <Icon height={16} icon="material-symbols:send-outline" />
-          </ActionIcon>
-        }
-        size="lg"
-        {...register('prompt', {
-          validate: {
-            notEmpty: (value: string) => value.trim().length > 0,
-            notBusy: () => !isBusy,
-          },
-        })}
-      />
+      <Stack>
+        <TextInput
+          rightSection={
+            <ActionIcon
+              color="blue"
+              loading={isBusy}
+              size="md"
+              type="submit"
+              variant="light"
+            >
+              <Icon height={16} icon="material-symbols:send-outline" />
+            </ActionIcon>
+          }
+          size="lg"
+          {...register('prompt', {
+            validate: {
+              notEmpty: (value: string) => value.trim().length > 0,
+              notBusy: () => !isBusy,
+            },
+          })}
+        />
+        {allowSystemMessage && (
+          <Checkbox
+            label="Set as system instruction"
+            {...register('asSystemMessage')}
+          />
+        )}
+      </Stack>
     </form>
   );
 };
