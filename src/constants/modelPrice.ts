73d260cd47c198d096c93455358be6ceb0f756e5
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-12-01 09:58:35 +0700

    refactor: model slice

--- src/constants/modelPrice.ts
+++ src/constants/modelPrice.ts
@@ -1,20 +1,40 @@
-import type { TModel } from '@/store/slice/modelSlice';
+import type { TModel, TModelType } from '@/store/slice/modelSlice';
 
 export const MODEL_PRICE = [
   {
+    type: 'chat',
     name: 'gpt-3.5-turbo',
     price: {
-      prompt: 0.0002,
-      completion: 0.0002,
+      in: { value: 0.0002, per: '1K tokens' },
+      out: { value: 0.0002, per: '1K tokens' },
     },
-    per: 1000,
+    provider: 'OpenAI',
   },
   {
+    type: 'chat',
     name: 'gpt-3.5-turbo-0301',
     price: {
-      prompt: 0.0002,
-      completion: 0.0002,
+      in: { value: 0.0002, per: '1K tokens' },
+      out: { value: 0.0002, per: '1K tokens' },
     },
-    per: 1000,
+    provider: 'OpenAI',
   },
-] satisfies Array<TModel>;
+  {
+    type: 'chat',
+    name: 'ggml-gpt4all-j-v1.3-groovy.bin',
+    price: {
+      in: { value: 0, per: 'tokens' },
+      out: { value: 0, per: 'tokens' },
+    },
+    provider: 'Self-hosted',
+  },
+  {
+    type: 'audio',
+    name: 'ggml-whisper-base.bin',
+    price: {
+      in: { value: 0, per: 'minute' },
+      out: { value: 0, per: 'minute' },
+    },
+    provider: 'Self-hosted',
+  },
+] as const satisfies readonly TModel<TModelType>[];
