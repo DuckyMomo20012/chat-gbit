f0e40737281e25598ac0580fd75e1dd98103e345
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-12-06 12:34:48 +0700

    refactor: migrate from redux to new api

--- src/pages/[[...slug]].tsx
+++ src/pages/[[...slug]].tsx
@@ -1,22 +1,15 @@
 import { Icon } from '@iconify/react';
 import { Button, Stack, Text } from '@mantine/core';
-import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import axios, { AxiosError } from 'axios';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { type OpenAI } from 'openai';
-import { useMemo, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useCallback, useMemo, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Convo } from '@/components/modules/Convo';
 import { PromptForm } from '@/components/modules/PromptForm';
 import { VoiceForm } from '@/components/modules/VoiceForm';
-import {
-  addCompletion,
-  addPrompt,
-  addSystemMessage,
-  clearTypingMessage,
-  removeMessage,
-  selectAllConvo,
-} from '@/store/slice/convoSlice';
 import type { RootState } from '@/store/store';
 
 export type TPromptForm = {
@@ -25,77 +18,170 @@ export type TPromptForm = {
 };
 
 const HomePage = () => {
-  const chat = useSelector(selectAllConvo);
-  const lastMessage = useSelector((state: RootState) =>
-    selectAllConvo(state).at(-1),
-  );
-  const model = useSelector((state: RootState) => state.model);
-  const isTyping = chat.filter((item) => item.isTyping).length > 0;
-  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const { slug } = router.query;
+  const id = slug?.at(0);
+
+  const currModel = useSelector((state: RootState) => state.model);
 
   const [inputMode, setInputMode] = useState<'voice' | 'text'>('text');
 
-  const {
-    data: completion,
-    isPending,
-    mutate,
-  } = useMutation({
-    mutationFn: async (): Promise<OpenAI.Chat.ChatCompletion> => {
-      const { data } = await axios.post('/api/completions', {
-        data: {
-          model: model.chat.name,
-          messages: chat.map((item) => ({
-            role: item.role,
-            content: item.content,
-          })),
-        },
+  const getConvoId = useCallback(async () => {
+    if (!id) {
+      const { data: convo } = await axios.post(`/api/conversations`, {
+        title: 'Untitled',
       });
+
+      await router.push(`/${convo.id}`);
+
+      return convo.id;
+    }
+
+    return id;
+  }, [id, router]);
+
+  const queryClient = useQueryClient();
+
+  const { data: conversation, isFetching } = useQuery({
+    // NOTE: router.query.slug changes make the query refetch, not the "id", so
+    // we have to pass all the slug to the queryKey
+    queryKey: ['conversations', router.query.slug],
+    queryFn: async () => {
+      try {
+        // NOTE: Handle root path
+        if (!id) return [];
+
+        const { data } = await axios.get(`/api/conversations/${id}`);
+
+        return data;
+      } catch (err) {
+        if (err instanceof AxiosError) {
+          throw err;
+        }
+      }
+
+      return [];
+    },
+  });
+
+  const lastMessage = conversation?.messages && conversation.messages.at(-1);
+
+  const { isPending: isFetchingCompletions, mutate: getCompletions } =
+    useMutation({
+      mutationFn: async ({
+        model,
+        conversationId,
+      }: {
+        model: string;
+        conversationId: string;
+      }): Promise<OpenAI.Chat.ChatCompletion> => {
+        const { data } = await axios.post(
+          `/api/conversations/${conversationId}/completions`,
+          {
+            model,
+            messages:
+              conversation?.messages.map(
+                ({
+                  role,
+                  content,
+                }: {
+                  role: 'user' | 'assistant' | 'system';
+                  content: string;
+                }) => ({
+                  role,
+                  content,
+                }),
+              ) || [],
+          },
+        );
+
+        return data;
+      },
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: ['conversations', router.query.slug],
+        });
+      },
+    });
+
+  const { isPending: isSubmittingPrompt, mutate: submitPrompt } = useMutation({
+    mutationFn: async ({
+      role,
+      content,
+      conversationId,
+    }: {
+      role: 'user' | 'system';
+      content: string;
+      conversationId: string;
+    }): Promise<OpenAI.Chat.ChatCompletion> => {
+      const { data } = await axios.post(
+        `/api/conversations/${conversationId}/prompt`,
+        {
+          role,
+          content,
+        },
+      );
 
       return data;
     },
-    onSuccess: (data) => {
-      if (data.choices[0].message?.content) {
-        dispatch(
-          addCompletion({
-            ...completion,
-            content: data.choices[0].message?.content,
-            isTyping: true,
-          }),
-        );
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['conversations', router.query.slug],
+      });
+
+      if (id) {
+        getCompletions({
+          model: currModel.chat.name,
+          conversationId: id,
+        });
       }
     },
   });
 
-  const isBusy = isPending || isTyping;
+  const { isPending: isRegenerating, mutate: regenerate } = useMutation({
+    mutationFn: async (): Promise<OpenAI.Chat.ChatCompletion> => {
+      const { data } = await axios.post(`/api/conversations/${id}/regenerate`, {
+        model: currModel.chat.name,
+      });
+
+      return data;
+    },
+    onSuccess: () => {
+      // NOTE: Invalidate the query, because the prompt still created even if error
+      queryClient.invalidateQueries({
+        queryKey: ['conversations', router.query.slug],
+      });
+    },
+  });
+
+  const isBusy =
+    isSubmittingPrompt || isFetchingCompletions || isRegenerating || isFetching;
 
   const allowRegenerate = useMemo(() => {
     return !isBusy && lastMessage && lastMessage.role !== 'system';
   }, [lastMessage, isBusy]);
 
-  const allowSystemMessage = chat.length === 0;
+  const allowSystemMessage = conversation?.messages?.length === 0;
 
-  const submitPrompt = async (data: TPromptForm) => {
+  const handleSubmit = async (data: TPromptForm) => {
     if (isBusy) return;
 
+    const conversationId = await getConvoId();
+
     if (data?.asSystemMessage) {
-      dispatch(
-        addSystemMessage({
-          content: data.prompt,
-          isTyping: false,
-        }),
-      );
-      return;
-    }
-
-    dispatch(
-      addPrompt({
+      submitPrompt({
+        role: 'system',
         content: data.prompt,
-        isTyping: false,
-      }),
-    );
-
-    // NOTE: Submit prompt
-    mutate();
+        conversationId,
+      });
+    } else {
+      submitPrompt({
+        role: 'user',
+        content: data.prompt,
+        conversationId,
+      });
+    }
   };
 
   return (
@@ -108,11 +194,11 @@ const HomePage = () => {
         <meta content="Create new Chat GBiT" name="description"></meta>
       </Head>
 
-      <Convo chat={chat} isFetching={isPending} />
+      <Convo chat={conversation?.messages} isFetching={isFetchingCompletions} />
 
       <Stack className="absolute bottom-0 z-[100] w-screen pb-4">
         <Stack align="center" className="p-4 backdrop-blur-xl backdrop-filter">
-          {isTyping && (
+          {/* {isTyping && (
             <Button
               leftSection={
                 <Icon
@@ -128,7 +214,7 @@ const HomePage = () => {
             >
               Stop generating
             </Button>
-          )}
+          )} */}
 
           {allowRegenerate && (
             <Button
@@ -139,15 +225,7 @@ const HomePage = () => {
                   width={16}
                 />
               }
-              onClick={() => {
-                // NOTE: Remove last message if it's assistant's message before
-                // regenerating
-                if (lastMessage?.role === 'assistant') {
-                  dispatch(removeMessage(lastMessage.id));
-                }
-
-                mutate();
-              }}
+              onClick={() => regenerate()}
               variant="outline"
             >
               Regenerate response
@@ -181,7 +259,7 @@ const HomePage = () => {
               <PromptForm
                 allowSystemMessage={allowSystemMessage}
                 isBusy={isBusy}
-                submitPrompt={submitPrompt}
+                submitPrompt={handleSubmit}
               />
             )}
 
@@ -189,7 +267,7 @@ const HomePage = () => {
               <VoiceForm
                 allowSystemMessage={allowSystemMessage}
                 isBusy={isBusy}
-                submitPrompt={submitPrompt}
+                submitPrompt={handleSubmit}
               />
             )}
           </Stack>
