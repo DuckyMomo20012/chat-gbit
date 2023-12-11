e1b6a45292a39b685ff716041c3b77e4a90613b2
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-12-11 20:21:28 +0700

    refactor: typed message & message component

--- src/components/elements/TypedMessage.tsx
+++ src/components/elements/TypedMessage.tsx
@@ -1,79 +1,112 @@
-import { useRef } from 'react';
+import {
+  type ForwardedRef,
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
 import Typed, { type TypedOptions } from 'typed.js';
 import { Message, type TMessageProp } from '@/components/elements/Message';
 
-const TypedMessage = ({
-  loop = false,
-  typedOptions,
-  isTyping,
-  ...props
-}: {
-  loop?: boolean;
-  typedOptions?: (
-    typedValRef: React.MutableRefObject<string | null>,
-  ) => TypedOptions;
-} & TMessageProp) => {
-  const typingsRef = useRef<Typed | null>(null);
+export type TTypedMessageHandle = {
+  start: () => void;
+  stop: () => string | null | undefined;
+};
 
-  const mutationObserverRef = useRef<MutationObserver | null>(null);
-  const typedValRef = useRef<string | null>(null);
+const TypedMessage = forwardRef(function TypedMessage(
+  {
+    content,
+    typedOptions,
+    isTyping,
+    ...props
+  }: {
+    isTyping?: boolean;
+    typedOptions?: (
+      messageRef: React.MutableRefObject<HTMLSpanElement | null>,
+    ) => TypedOptions;
+  } & TMessageProp,
+  ref: ForwardedRef<TTypedMessageHandle>,
+) {
+  const typedRef = useRef<Typed | null>(null);
+  const messageRef = useRef<HTMLSpanElement>(null);
 
-  return (
-    <Message
-      isTyping={isTyping}
-      ref={(node: HTMLSpanElement) => {
-        // NOTE: Component unmounting, so we need to clean up.
-        if (!node) {
-          typingsRef.current?.destroy();
-          mutationObserverRef.current?.disconnect();
-          return;
-        }
+  // NOTE: We provide content as the default value for the typedContent state,
+  // so for non-typed message, it will be rendered immediately.
+  const [typedContent, setTypedContent] = useState<string>(content);
 
-        const observer = new MutationObserver((mutations) => {
-          mutations.forEach((mutation) => {
-            if (mutation.type === 'childList') {
-              typedValRef.current = mutation.addedNodes[0]?.textContent;
-            }
-          });
-        });
+  useEffect(() => {
+    if (!messageRef.current) {
+      return () => {};
+    }
 
-        observer.observe(node, {
-          childList: true,
-        });
+    if (!isTyping) {
+      return () => {};
+    }
 
-        mutationObserverRef.current = observer;
-
-        const typed = new Typed(node, {
+    const typed = new Typed(messageRef.current, {
+      // NOTE: A little hacky, we pause the typing for 1ms to
+      // trigger the onTypingPaused event.
+      strings: [
+        content
+          // Ref: https://stackoverflow.com/a/6234804/12512981
+          .replace(/&/g, '&amp;') // NOTE: Must be done first!
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;')
+          .replace(/"/g, '&quot;')
+          .replace(/'/g, '&#039;')
+          // NOTE: Replace the backticks with HTML codes to prevent the
+          // Typed.js library from ignoring the backticks. Don't worry,
+          // on browser, the backticks will be rendered correctly, not
+          // the HTML codes.
+          .replace(/`/g, '&#96;')
           // NOTE: A little hacky, we pause the typing for 1ms to
           // trigger the onTypingPaused event.
-          strings: [
-            props.content
-              // Ref: https://stackoverflow.com/a/6234804/12512981
-              .replace(/&/g, '&amp;') // NOTE: Must be done first!
-              .replace(/</g, '&lt;')
-              .replace(/>/g, '&gt;')
-              .replace(/"/g, '&quot;')
-              .replace(/'/g, '&#039;')
-              // NOTE: Replace the backticks with HTML codes to prevent the
-              // Typed.js library from ignoring the backticks. Don't worry,
-              // on browser, the backticks will be rendered correctly, not
-              // the HTML codes.
-              .replace(/`/g, '&#96;')
-              // NOTE: A little hacky, we pause the typing for 1ms to
-              // trigger the onTypingPaused event.
-              .replace(/([^\s]+)/g, '^1 `$1`'),
-          ],
-          typeSpeed: 100,
-          cursorChar: '█',
-          loop,
-          ...(typedOptions && typedOptions(typedValRef)),
-        });
+          .replace(/([^\s]+)/g, '^1 `$1`'),
+      ],
+      typeSpeed: 100,
+      cursorChar: '█',
+      ...(typedOptions && typedOptions(messageRef)),
 
-        typingsRef.current = typed;
-      }}
-      {...props}
-    />
+      onTypingPaused: () => {
+        setTypedContent(messageRef.current?.textContent || '');
+      },
+    });
+
+    typedRef.current = typed;
+
+    return () => {
+      typed.destroy();
+    };
+  }, [content, typedOptions, isTyping]);
+
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        start: () => {
+          typedRef.current?.start();
+        },
+        stop: () => {
+          typedRef.current?.stop();
+
+          // NOTE: We use the ref to get the textContent instead of the state,
+          // so it will always return the latest textContent
+          return messageRef.current?.textContent;
+        },
+      };
+    },
+    [],
   );
-};
+
+  return isTyping ? (
+    <Message content="" ref={messageRef} {...props} />
+  ) : (
+    // NOTE: We use the typedContent state instead of the content prop, so it
+    // won't have flashing effect that show entire content after the typing is
+    // done.
+    <Message content={typedContent} {...props} />
+  );
+});
 
 export { TypedMessage };
