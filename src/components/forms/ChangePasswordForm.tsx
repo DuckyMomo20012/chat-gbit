1d4c6e01bd760c667d8f841460a7af0964853b10
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-12-16 11:04:21 +0700

    feat(web): edit user & change password

---
+++ src/components/forms/ChangePasswordForm.tsx
@@ -0,0 +1,96 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Group, PasswordInput, Space, Stack } from '@mantine/core';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+import { signOut } from 'next-auth/react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+export const changePasswordSchema = z
+  .object({
+    password: z.string().optional(),
+    confirmPassword: z.string().optional(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+  });
+
+export type TChangePasswordForm = z.infer<typeof changePasswordSchema>;
+
+const ChangePasswordForm = ({ userId }: { userId?: string }) => {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateUser } = useMutation({
+    mutationKey: ['users', 'update', userId],
+    mutationFn: async (data: TChangePasswordForm) => {
+      const { data: user } = await axios.patch(
+        `/api/users/${userId}/change-password`,
+        {
+          password: data.password,
+        },
+      );
+
+      return user;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['user', userId],
+      });
+
+      // NOTE: We need to sign out the user after changing the password because
+      // the session is no longer valid.
+      signOut({
+        redirect: false,
+        callbackUrl: '/auth/sign-in',
+      });
+    },
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isDirty },
+  } = useForm<TChangePasswordForm>({
+    defaultValues: {
+      password: '',
+      confirmPassword: '',
+    },
+    resolver: zodResolver(changePasswordSchema),
+  });
+
+  const onSubmit = (data: TChangePasswordForm) => {
+    updateUser(data);
+  };
+
+  return (
+    <form className="h-full w-full" onSubmit={handleSubmit(onSubmit)}>
+      <Stack>
+        <PasswordInput
+          error={errors.password?.message}
+          label="Password"
+          {...register('password')}
+        />
+        <PasswordInput
+          error={errors.confirmPassword?.message}
+          label="Confirm password"
+          {...register('confirmPassword')}
+        />
+
+        <Space h="md" />
+
+        <Group justify="center">
+          <Button disabled={!isDirty} onClick={() => reset()} variant="outline">
+            Discard
+          </Button>
+          <Button disabled={!isDirty} type="submit">
+            Update
+          </Button>
+        </Group>
+      </Stack>
+    </form>
+  );
+};
+
+export { ChangePasswordForm };
---
+++ src/components/forms/ProfileForm.tsx
@@ -0,0 +1,91 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Group, Space, Stack, TextInput } from '@mantine/core';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { userBodySchema } from '@/pages/api/users';
+
+export const profileSchema = userBodySchema.pick({ name: true });
+
+export type TProfileForm = z.infer<typeof profileSchema>;
+
+const ProfileForm = ({ userId }: { userId?: string }) => {
+  const queryClient = useQueryClient();
+
+  const { data: userInfo } = useQuery({
+    queryKey: ['user', userId],
+    queryFn: async () => {
+      const { data } = await axios.get(`/api/users/${userId}`);
+
+      return data;
+    },
+    enabled: !!userId,
+  });
+
+  const { mutate: updateUser } = useMutation({
+    mutationKey: ['users', 'update', userId],
+    mutationFn: async (data: TProfileForm) => {
+      const { data: user } = await axios.patch(`/api/users/${userId}`, {
+        name: data.name,
+      });
+
+      return user;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['user', userId],
+      });
+    },
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isDirty },
+  } = useForm<TProfileForm>({
+    defaultValues: {
+      name: '',
+    },
+    resolver: zodResolver(profileSchema),
+  });
+
+  useEffect(() => {
+    if (userInfo) {
+      reset({
+        name: userInfo.name,
+      });
+    }
+  }, [userInfo, reset]);
+
+  const onSubmit = (data: TProfileForm) => {
+    updateUser(data);
+  };
+
+  return (
+    <form className="h-full w-full" onSubmit={handleSubmit(onSubmit)}>
+      <Stack>
+        <TextInput
+          error={errors.name?.message}
+          label="Name"
+          {...register('name')}
+        />
+
+        <Space h="md" />
+
+        <Group justify="center">
+          <Button disabled={!isDirty} onClick={() => reset()} variant="outline">
+            Discard
+          </Button>
+          <Button disabled={!isDirty} type="submit">
+            Update
+          </Button>
+        </Group>
+      </Stack>
+    </form>
+  );
+};
+
+export { ProfileForm };
