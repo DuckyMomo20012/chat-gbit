3b69a368826055c5aa8082bf761252c58428f021
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-12-18 17:43:21 +0700

    refactor: rename conversation schema to chat
    fix: update incorrect field naming

---
+++ prisma/migrations/20231218104315_rename_conversation_to_chat/migration.sql
@@ -0,0 +1,37 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `conversationId` on the `Message` table. All the data in the column will be lost.
+  - You are about to drop the `Conversation` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `chatId` to the `Message` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Conversation" DROP CONSTRAINT "Conversation_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Message" DROP CONSTRAINT "Message_conversationId_fkey";
+
+-- AlterTable
+ALTER TABLE "Message" DROP COLUMN "conversationId",
+ADD COLUMN     "chatId" TEXT NOT NULL;
+
+-- DropTable
+DROP TABLE "Conversation";
+
+-- CreateTable
+CREATE TABLE "Chat" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "Chat"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
