f4a392247e811eb100f363d5af3678a435f85781
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-12-25 21:48:40 +0700

    refactor(web): migrate from next/head to new metadata app dir

--- src/app/(protected)/(chat)/[[...slug]]/page.tsx
+++ src/app/(protected)/(chat)/[[...slug]]/page.tsx
@@ -1,5 +1,32 @@
 import { Stack } from '@mantine/core';
+import { type Metadata } from 'next';
+import { getOneChat } from '@/app/api/users/[userId]/chat/[chatId]/route';
 import { ChatController } from '@/components/modules/ChatController';
+import { auth } from '@/lib/auth';
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { slug: string[] };
+}): Promise<Metadata> => {
+  const session = await auth();
+  const userId = session?.user?.id;
+  const chatId = params?.slug?.at(0);
+
+  try {
+    const chat = await getOneChat(userId as string, chatId as string);
+
+    return {
+      title: chat.title,
+      description: chat.title,
+    };
+  } catch (err) {
+    return {
+      title: 'Chat GBiT',
+      description: 'Create new chat',
+    };
+  }
+};
 
 const HomePage = () => {
   return (
