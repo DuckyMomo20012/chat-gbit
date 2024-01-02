38e25e793d24469b631a6b653ce6ec028489aceb
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2024-01-02 09:14:51 +0700

    chore: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,42 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import twContainerQueries from '@tailwindcss/container-queries';
 import type { Config } from 'tailwindcss';
-import twDefaultColors from 'tailwindcss/colors';
 
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   darkMode: ['class', '[data-mantine-color-scheme="dark"]'],
-  // NOTE: Ah, tailwindcss utilities can't beat antd styles.
-  important: true,
   theme: {
     extend: {
       colors: {
-        dark: twDefaultColors.slate,
-        grape: twDefaultColors.fuchsia,
+        dark: {
+          '50': '#f8fafc',
+          '100': '#f1f5f9',
+          '200': '#e2e8f0',
+          '300': '#cbd5e1',
+          '400': '#94a3b8',
+          '500': '#64748b',
+          '600': '#475569',
+          '700': '#334155',
+          '800': '#1e293b',
+          '900': '#0f172a',
+          '950': '#020617',
+        },
+        grape: {
+          '50': '#fdf4ff',
+          '100': '#fae8ff',
+          '200': '#f5d0fe',
+          '300': '#f0abfc',
+          '400': '#e879f9',
+          '500': '#d946ef',
+          '600': '#c026d3',
+          '700': '#a21caf',
+          '800': '#86198f',
+          '900': '#701a75',
+          '950': '#4a044e',
+        },
       },
     },
     screens: {
